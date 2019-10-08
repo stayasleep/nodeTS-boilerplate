ba884736397b891a656ea540043921aea4cdfe86
stayasleep <virtuallyinfekted@gmail.com>
2019-10-07 21:28:25 -0700

    add type to describe what what kind of func wrapper util takes in

--- src/utils/apiWrapper.ts
+++ src/utils/apiWrapper.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
-export const apiWrapper = (callback: Function) => {
+type AsyncFunction<A, O> = (...args: Array<A>) => O;
+
+export const apiWrapper = (callback: AsyncFunction<any, void>) => {
     return async function(req: Request, res: Response, next: NextFunction) {
         try {
             return await callback(req, res, next);
