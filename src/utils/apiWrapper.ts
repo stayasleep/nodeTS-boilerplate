f3a3e277a1b1e051bb106988e6c99e34d913cc15
stayasleep <virtuallyinfekted@gmail.com>
2019-10-16 02:17:52 -0700

    removed refs to s3 deployment script and add sls deploy for lambda instead

--- src/utils/apiWrapper.ts
+++ src/utils/apiWrapper.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 type AsyncFunction<A, O> = (...args: Array<A>) => O;
+type A = Response | Request | NextFunction;
 
-export const apiWrapper = (callback: AsyncFunction<any, void>) => {
+export const apiWrapper = (callback: AsyncFunction<A, void>) => {
     return async function(req: Request, res: Response, next: NextFunction) {
         try {
             return await callback(req, res, next);
